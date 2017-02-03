74829aea032f8db5c5abd8d0a7e40fd2aa434861
Roy McKenzie <roy@propellerlabs.co>
2017-02-03 14:40:27 -0800

    Initial Commit

---
+++ PropellerKit/PropellerKit.h
@@ -0,0 +1,19 @@
+//
+//  PropellerKit.h
+//  PropellerKit
+//
+//  Created by Roy McKenzie on 2/3/17.
+//  Copyright © 2017 Propeller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PropellerKit.
+FOUNDATION_EXPORT double PropellerKitVersionNumber;
+
+//! Project version string for PropellerKit.
+FOUNDATION_EXPORT const unsigned char PropellerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PropellerKit/PublicHeader.h>
+
+
